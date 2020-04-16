4e26705720d61497c792d0cd0bea8c085f231621
sjjeong <sjjeong1225@gmail.com>
2020-04-17 00:13:17 +0900

    SecondFragment navigate

--- app/src/main/java/com/dino/navigationstudy/MainFragment.kt
+++ app/src/main/java/com/dino/navigationstudy/MainFragment.kt
@@ -1,5 +1,18 @@
 package com.dino.navigationstudy
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_main.view.*
 
-class MainFragment : Fragment(R.layout.fragment_main)
+class MainFragment : Fragment(R.layout.fragment_main){
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.btn_move.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_secondFragment)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/dino/navigationstudy/SecondFragment.kt
@@ -0,0 +1,5 @@
+package com.dino.navigationstudy
+
+import androidx.fragment.app.Fragment
+
+class SecondFragment : Fragment(R.layout.fragment_second)
